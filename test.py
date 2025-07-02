0ebbae9117f5a9b81be3ea11ac7df30ab419d9b1
otsuka0029 <166772480+otsuka0029@users.noreply.github.com>
2025-07-02 03:16:09 +0000

    modify test.py(2)

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import math, sys;
 
 def example1():
     ####This is a long comment. This should be wrapped to fit within 72 characters.
-    some_tuple=(   1,2, 5,'a'  );
+    some_tuple=(   1,2,3,'a'  );
     some_variable={'long':'Long code lines should be wrapped within 79 characters.',
     'other':[math.pi, 100,200,300,9876543210,'This is a long string that goes on'],
     'more':{'inner':'This whole logical line should be wrapped.',some_tuple:[1,
